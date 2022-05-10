e55894092cf036c9a9f12ddb3fe49260feb9d65e
Nick B <nbourrereg@hotmail.com>
2022-05-10 12:53:15 -0400

    direction flip done

--- yt_plateforme_rapide/Mob.gd
+++ yt_plateforme_rapide/Mob.gd
@@ -10,6 +10,9 @@ var current_state = WALKING
 
 var velocity = Vector2.ZERO
 
+var direction = 1
+var direction_flip = false
+
 func _ready():
 	pass
 
@@ -26,10 +29,14 @@ func _physics_process(delta):
 
 
 func walking(delta):
-	
+	detect_direction_change()
+	if direction_flip:
+		direction_flip = false
+		scale.x *= -1
+
 	velocity.y += GRAVITY if velocity.y < MAX_FALL_VELOCITY else MAX_FALL_VELOCITY
 
-	velocity.x = MAX_VELOCITY
+	velocity.x = MAX_VELOCITY * direction
 
 	velocity = move_and_slide(velocity, Vector2.UP)
 
@@ -38,3 +45,12 @@ func attacking(delta):
 
 func death(delta):
 	pass
+
+func detect_direction_change():
+	if is_on_floor():
+		if not $FloorDetector.is_colliding() or $WallDetector.is_colliding():
+			flip_direction()
+
+func flip_direction():
+	direction *= -1
+	direction_flip = true
