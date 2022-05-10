5b10b105ec8f24f0ce5619b9fc86d8a2a462336c
Nick B <nbourrereg@hotmail.com>
2022-05-10 11:32:22 -0400

    Écriture du script

---
+++ yt_plateforme_rapide/Mob.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+const MAX_VELOCITY = 50
+const MAX_FALL_VELOCITY = 200
+const GRAVITY = 10
+
+enum {WALKING, ATTACKING, DEATH}
+
+var current_state = WALKING
+
+var velocity = Vector2.ZERO
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+
+	match(current_state):
+		WALKING:
+			walking(delta)
+		ATTACKING:
+			attacking(delta)
+		DEATH:
+			death(delta)
+			return
+	
+	velocity.y += GRAVITY if velocity.y < MAX_FALL_VELOCITY else MAX_FALL_VELOCITY
+
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func walking(delta):
+	pass
+
+func attacking(delta):
+	pass
+
+func death(delta):
+	pass
